448afc4abed2fca1624734faebfb5c9787992158
Tyler Dunham <tylerjdunham1@gmail.com>
2024-10-07 02:57:45 -0400

    initial final product. needs better styling

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import os
 import discord
 from dotenv import load_dotenv
-import schedule
-import time
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import asyncio
 from job import job
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL = os.getenv('CHANNEL_ID')
+CHANNEL = int(os.getenv('CHANNEL_ID'))
 
-#dumb windows problem where it needs different loop policy
+# dumb windows problem where it needs different loop policy
 import asyncio
 if os.name == 'nt':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -21,17 +21,27 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    scheduler.start()
 
 async def send_internships():
     channel = client.get_channel(CHANNEL)
+    if channel is None:
+        print(f"Invalid channel id {CHANNEL}")
+        return
+
     internships = job()
-    for internship in internships:
-        await channel.send(internship)
+    for key, internship in internships.items():
+        message = (
+            f"Company: {internship['company']}\n"
+            f"Position: {internship['position']}\n"
+            f"Locations: {internship['location']}\n"
+            f"URL: <{internship['url']}>\n"
+            f"----------------------------------"
+        )
+        await channel.send(message)
+    print("Sent internships")
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(send_internships, 'cron', hour=23, minute=59)
 
 client.run(TOKEN)
-
-# Cron every day at midnight
-schedule.every(5).seconds.do(send_internships)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
--- job.py
+++ job.py
@@ -2,38 +2,96 @@ import requests
 import os
 from datetime import datetime
 from dotenv import load_dotenv
+import re
+from bs4 import BeautifulSoup
+import pytz
 
+# env vars, headers, and timezone
 load_dotenv()
 owner = os.getenv('OWNER')
 repo = os.getenv('REPO')
-current_date = datetime.now().date().isoformat() + 'T00:00:00Z'
+github_token = os.getenv('GITHUB_TOKEN')
+headers = {
+    'Authorization': f'token {github_token}'
+}
+local_tz = pytz.timezone('America/New_York')
 
 def job():
-    parsed_internships = []
-    commits = get_commits()    
+    # get the current date
+    local_time = datetime.now(local_tz).replace(hour=0, minute=0, second=0, microsecond=0)
+    current_date = local_time.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
+
+    parsed_internships = {}
+    errors = 0
+    commits = get_commits(start_date=current_date)    
     print(f"Found {len(commits)} commits")
 
     for commit in commits:
         try:
-            parsed_internship = parse_commit(commit)
-            print(parsed_internship)
-            parsed_internships.append(parsed_internship)
+            sha = commit['sha']
+            commit_data = get_commit(sha)
+            parsed_internship = parse_commit(commit_data)
+            company = parsed_internship['company']
+            position = parsed_internship['position']
+            intern_key = f"{company}:{position}"
+            if intern_key not in parsed_internships:
+                parsed_internships[intern_key] = parsed_internship
         except Exception as e:
-            print(f"Probably rate limited")
+            errors += 1 
 
+    print(f"Found {len(parsed_internships)} internships. Removed {errors} due to errors.")
     return parsed_internships
 
-
-def get_commits(start_date=current_date, end_date=None):
+def get_commits(start_date=None, end_date=None):
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    params = {}
-    params['since'] = start_date
-    params['until'] = end_date
-    response = requests.get(url, params=params)
+    params = {
+        'since': start_date,
+        'until': end_date
+    }
+    response = requests.get(url, params=params, headers=headers)
+    return response.json()
+
+def get_commit(sha: str):
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits/{sha}"
+    response = requests.get(url, headers=headers)
     return response.json()
 
 def parse_commit(commit):
-    return [commit['files'][1]['patch'], commit['files'][0]['filename']]
+    # readme of the commit
+    diff_string = commit['files'][1]['patch'] if len(commit['files']) > 1 else commit['files'][0]['patch']
+
+    # regex for splitting the diff string
+    pattern = r'\n\+(.*?)\n'
+    match = re.search(pattern, diff_string)
+    
+    if match:
+        # Return the captured group (everything between '\n+' and '\n')
+        raw = match.group(1)
+    else:
+        return None
+    
+    split_raw = raw.split('|')
+
+    # company, position, location, url
+    company = split_raw[1].strip()
+    position = split_raw[2].strip()
+
+    url_pattern = r'href="([^"]+)"'
+    url_match = re.search(url_pattern, split_raw[4])
+    url = url_match.group(1) if url_match else None
+
+    location_html = split_raw[3]
+    soup = BeautifulSoup(location_html, 'html.parser')
+    locations = [loc.strip() for loc in soup.stripped_strings]
+
+    # make parsed_internship object
+    parsed_internship = {
+        'company': company,
+        'position': position,
+        'location': locations,
+        'url': url
+    }
+    return parsed_internship
 
 if __name__ == "__main__":
     job()
