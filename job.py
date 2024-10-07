0f18ac9faeca17d465ed482058c99fa28e946653
Tyler Dunham <tylerjdunham1@gmail.com>
2024-10-06 23:17:46 -0400

    error handling

--- job.py
+++ job.py
@@ -6,19 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 owner = os.getenv('OWNER')
 repo = os.getenv('REPO')
-current_date = datetime.now().isoformat() + 'Z'
+current_date = datetime.now().date().isoformat() + 'T00:00:00Z'
 
 def job():
     parsed_internships = []
-    commits = get_commits()
+    commits = get_commits()    
     print(f"Found {len(commits)} commits")
+
     for commit in commits:
-        sha = commit['sha']
-        print(sha)
-        commit_data = get_commit(sha)
-        print(commit_data)
-        parsed_internship = parse_commit(commit_data)
-        parsed_internships.append(parsed_internship)
+        try:
+            sha = commit['sha']
+            commit_data = get_commit(sha)
+            print(commit_data)
+            parsed_internship = parse_commit(commit_data)
+            parsed_internships.append(parsed_internship)
+        except Exception as e:
+            print(f"Probably rate limited")
 
     return parsed_internships
 
@@ -32,12 +35,12 @@ def get_commits(start_date=current_date, end_date=None):
     return response.json()
 
 def get_commit(sha: str):
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits{sha}"
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits/{sha}"
     response = requests.get(url)
     return response.json()
 
 def parse_commit(commit):
-    return 5
+    return [commit['files'][0]['patch'], commit['files'][0]['filename']]
 
 if __name__ == "__main__":
     job()
